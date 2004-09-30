21026e24a69634979608faf9de73867d0a660785
djm <devnull@localhost>
2004-09-30 04:37:11 +0000

    - (djm) Release 0.9.6

--- common.h
+++ common.h
@@ -72,7 +72,7 @@
 #define PROGNAME		"softflowd"
 
 /* The name of the program */
-#define PROGVER			"0.9.2"
+#define PROGVER			"0.9.6"
 
 /* Default pidfile */
 #define DEFAULT_PIDFILE		"/var/run/" PROGNAME ".pid"
